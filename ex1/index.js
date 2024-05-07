71942d6e6720e2ff3f458c5ec71bb36948ab66c2
MariusS81 <mariusvilaescu@yahoo.com>
2024-05-07 20:38:18 +0300

    tema1 completed

---
+++ ex1/index.js
@@ -0,0 +1,13 @@
+const este_divizibil_cu10 = (numar) => {
+    if (numar %10 ===0) {
+      return true
+    }
+    else{
+      return false
+    } 
+     
+  }
+  
+  
+  console.log(este_divizibil_cu10(100))
+  console.log(este_divizibil_cu10(23))

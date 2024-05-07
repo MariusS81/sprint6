f0ca29d6c64d633f2007f35a12186670c3df0e00
MariusS81 <mariusvilaescu@yahoo.com>
2024-05-07 20:53:53 +0300

    tema2

---
+++ ex2/index.js
@@ -0,0 +1,5 @@
+function tellFortune(N, Z, Y, X) {
+    const tellFortune = `Vei fi un ${X} în ${Y}, căsătorit cu ${Z} și vei avea ${N} copii.`;
+      console.log(tellFortune);
+  }
+  tellFortune ("3", "Emanuel", "Italia", "Programator");
